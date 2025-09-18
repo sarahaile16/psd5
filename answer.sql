cd0b90997493fd96d92a165f318c7afaa646ce35
sarahaile16 <sarahailemariamsara5981@gmail.com>
2025-09-18 17:23:21 +0300

    Create answer.sql

---
+++ answer.sql
@@ -0,0 +1,4 @@
+DROP INDEX IdxPhone ON customers;
+CREATE USER 'bob'@'localhost' IDENTIFIED BY 'S$cu3r3!';
+GRANT INSERT ON salesDB.* TO 'bob'@'localhost';
+ALTER USER 'bob'@'localhost' IDENTIFIED BY 'P$55!23';
